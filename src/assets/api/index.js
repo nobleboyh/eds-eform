b3ecc319e6266ec1caca5e8e0d65e4009cbd2b07
HoangAtCode <hoangat3112@gmail.com>
2023-12-16 09:26:00 +0700

    add gh-pages

--- src/assets/api/index.js
+++ src/assets/api/index.js
@@ -1,5 +1,5 @@
 const api = {
-    script:"https://script.google.com/macros/s/AKfycbwbILPxzxDVkMv0ayJC7k0_TFs7fcngIWE5ads_u2WylAfxkOadcFUqZr-c-DAqYQDlwQ/exec",
+    script:"https://script.google.com/macros/s/AKfycbx_6Aq0c4RpBkyo4zrs2dpklkNVfSH5N1irXRcoCBOMH7YqizbIReIxSqclHZPhfg__/exec",
 }
 
 export default api;
